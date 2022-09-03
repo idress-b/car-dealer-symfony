2a55f10f013a5c3b03e82f0bb932a305dfd88291
idress-b <idress.benchellali@gmail.com>
2022-09-03 22:17:38 +0200

    trying to fix the post_submit event

--- src/Form/AnnoncesType.php
+++ src/Form/AnnoncesType.php
@@ -8,6 +8,7 @@ use App\Entity\Modeles;
 use App\Repository\ModelesRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -21,16 +22,23 @@ class AnnoncesType extends AbstractType
         $builder
             ->add('marque', EntityType::class, [
                 'class' => Marques::class,
-                
                 'placeholder' => '<< -choisissez- >>',
-              
+                'choice_label' => 'marque',
+                'mapped' => false,
+                'required' => false
+
+            ])
+            ->add('modele', EntityType::class, [
+                'class' => Modeles::class,
+                'placeholder' => 'choisir un modèle',
+                'required' => false
             ])
             ->add('title')
             ->add('subtitle')
             ->add('description')
             ->add('price')
             ->add('annee')
-            ->add('mec')
+            // ->add('mec')
             ->add('carburant')
             ->add('gearBox')
             ->add('type')
@@ -40,35 +48,39 @@ class AnnoncesType extends AbstractType
             ->add('cv')
             ->add('color')
             ->add('critAir');
-        // ->add('modele', EntityType::class, [
-        //     'class' => Modeles::class,
-        //     'placeholder' => '',
-        //     'disabled' => true
-        // ]);
+
 
         $formModifier = function (FormInterface $form, Marques $marque = null) {
-            $modele = null === $marque ? [] : $marque->getModeles();
-           
+
+            $modeles = null === $marque ? [] : $marque->getModeles();
+            dd($marque);
             $form->add('modele', EntityType::class, [
                 'class' => Modeles::class,
-                'placeholder' => 'placeholder',
-                'choices' => $modele,
+                'choices' => $modeles,
+                // 'query_builder' => function (ModelesRepository $repository, $int) {
+                //     return $repository->createQueryBuilder('m')
+                //         ->andWhere('m.marque = :val')
+                //         ->setParameter('val', $int);
+                // },
+
                 'required' => false
             ]);
         };
 
-        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($formModifier) {
 
-            $form = $event->getForm();
-            $data = $event->getData();
-          
-            $formModifier($form,$data->getMarque());
-        });
 
-        $builder->get('marque')->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) use ($formModifier) {
+        $builder->get('marque')->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
             $marque = $event->getForm()->getData();
-            
-            $formModifier($event->getForm()->getParent(), $marque);
+            $modeles = null === $marque ? [] : $marque->getModeles();
+            dd($marque);
+
+
+            $event->getForm()->getParent()
+                ->add('modele', EntityType::class, [
+                    'class' => Modeles::class,
+                    'choices' => $modeles,
+                    'required' => false
+                ]);
         });
     }
 
@@ -76,6 +88,7 @@ class AnnoncesType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Annonces::class,
+            'csrf_protection' => false
         ]);
     }
 }
